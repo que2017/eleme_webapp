a2f546d05a7a7a7aa485453f97057f888ae2cf91
zhangbx <bxzhang@iflytek.com>
2017-09-18 15:02:03 +0800

    feat(food组件): 完成展示动画

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="cover">
-    <div class="title-img"><img :src="food.image" width="100%" height="100%"/></div>
-    <div class="food-info"></div>
-  </div>
+  <transition name="fly-in">
+    <div class="cover">
+      <div class="close" @click="closeDetail">
+        <span class="icon-close"></span>
+      </div>
+      <div class="title-img"><img :src="food.image" width="100%" height="100%"/></div>
+      <div class="food-info"></div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -10,12 +15,22 @@
     props: {
       food: Object
     },
+    methods: {
+      closeDetail () {
+        this.$emit('close')
+      }
+    },
     created () {
     }
   }
 </script>
 
 <style lang="stylus">
+  .fly-in-enter-active, .fly-in-leave-active
+    transition: all 0.4s linear
+    transform: translate3d(0, 0, 0)
+  .fly-in-enter, .fly-in-leave-to
+    transform: translate3d(100%, 0, 0)
   .cover
     position: fixed
     top: 0
@@ -23,6 +38,18 @@
     bottom: 48px
     width: 100%
     background: #fff
+    .close
+      position: absolute
+      top: 10px
+      left: 10px
+      width: 32px
+      height: 32px
+      border-radius: 50%
+      color: #fff
+      text-align: center
+      line-height: 32px
+      font-size: 24px
+      background: rgba(0, 0, 0, 0.3)
     .title-img
       width: 100%
       height: 375px
